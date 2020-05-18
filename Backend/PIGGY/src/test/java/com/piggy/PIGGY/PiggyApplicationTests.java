4282bfa599aa6fa6250a32a19fcd4c82ca99b586
integeran <jmiha22@gmail.com>
2020-05-18 17:19:42 +0900

    S02P31A301-31 | develop | SA | settings backend

---
+++ Backend/PIGGY/src/test/java/com/piggy/PIGGY/PiggyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piggy.PIGGY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiggyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
