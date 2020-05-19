70b82d198ce5f2e376cc9dd57336738139ddf76b
chokyungeun <ruddms0127@naver.com>
2020-05-19 16:12:52 +0900

    S02P31A301-45 | feature/mainpage_front| KE | 하단 Navbar 완성

--- Frontend/piggylife/src/App.js
+++ Frontend/piggylife/src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { createGlobalStyle } from "styled-components";
 
 import LoginPage from "./pages/LoginPage";
 import JoinPage from "./pages/JoinPage";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
     <div>
       <GlobalStyle></GlobalStyle>
       <Switch>
+        <Route path="/home" component={HomePage} />
         <Route path="/join" component={JoinPage} />
         <Route path="/" component={LoginPage} />
       </Switch>
@@ -23,6 +25,5 @@ const GlobalStyle = createGlobalStyle`
     height: 100%;
   }
 `;
-
 
 export default App;
