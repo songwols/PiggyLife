1c6e924ad7ebc3df0f25c38074880454370eff81
chokyungeun <ruddms0127@naver.com>
2020-05-22 15:55:06 +0900

    S02P31A301-62 | feature/feedpage_front| KE | 더보기 버튼 추가, 프로필 고정

--- Frontend/piggylife/src/components/Profile/index.jsx
+++ Frontend/piggylife/src/components/Profile/index.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import styled from "styled-components";
 import { Email } from "@styled-icons/material-outlined/Email";
 import { PersonOutline } from "@styled-icons/evaicons-outline/PersonOutline";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { DotsVerticalRounded } from "@styled-icons/boxicons-regular/DotsVerticalRounded";
+
+export const More = styled(DotsVerticalRounded)`
+  width: 2rem;
+  height: 2rem;
+  float: right;
+  margin: 0.5rem;
+  color: black;
+`;
 
 export const EmailIcon = styled(Email)`
   width: 1rem;
@@ -20,28 +29,44 @@ class Profile extends React.Component {
   render() {
     return (
       <Frame>
-        <ProfileImage src="https://image.flaticon.com/icons/svg/747/747376.svg"></ProfileImage>
-        <Info>
-          <Nickname>
-            <PersonIcon></PersonIcon>
-            Nickname
-          </Nickname>
-          <Emaildiv>
-            <EmailIcon></EmailIcon>
-            ssafy@ssafy.com
-          </Emaildiv>
-        </Info>
+        <Div>
+          <ProfileImage src="https://image.flaticon.com/icons/svg/747/747376.svg"></ProfileImage>
+          <Info>
+            <Nickname>
+              <PersonIcon></PersonIcon>
+              Nickname
+            </Nickname>
+            <Emaildiv>
+              <EmailIcon></EmailIcon>
+              ssafy@ssafy.com
+            </Emaildiv>
+          </Info>
+        </Div>
+        <Link to={"/More"} style={{ textDecoration: "none" }}>
+          <More></More>
+        </Link>
       </Frame>
     );
   }
 }
 
 const Frame = styled.div`
+  position: sticky;
+  top: 0;
+  height: 150px;
+  display: grid;
+  grid-template-columns: auto 0.5fr;
+  grid-template-areas: "Div More";
+`;
+const Div = styled.div`
+  position: sticky;
+  top: 0;
   height: 150px;
   align-items: center;
   display: grid;
   grid-template-columns: 1fr 2fr;
   grid-template-areas: "ProfileImage Info";
+  margin-left: 1.5rem;
 `;
 
 const Info = styled.div`
