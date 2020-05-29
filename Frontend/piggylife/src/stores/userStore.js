59a498bfb10d3c7e9fa329c5b7a7c0cc4e991f01
chokyungeun <ruddms0127@naver.com>
2020-05-29 10:31:06 +0900

    S02P31A301-89 | feature/login_front| KE | 이메일 인증 기능 구현

--- Frontend/piggylife/src/stores/userStore.js
+++ Frontend/piggylife/src/stores/userStore.js
@@ -1,4 +1,5 @@
 import { observable, action } from "mobx";
+import agent from "../agent";
 
 export default class UserStore {
   @action
@@ -8,5 +9,29 @@ export default class UserStore {
   @action
   login(user) {
     console.log(user);
+  }
+  @action
+  email_check(email) {
+    return agent.Data.email_check(email)
+      .then((res) => {
+        console.log(res);
+        alert("사용가능한 이메일입니다.");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("이미 등록된 이메일입니다.");
+      });
+  }
+  @action
+  code_check(user) {
+    return agent.Data.code_check(user)
+      .then((res) => {
+        console.log(res);
+        alert("인증되었습니다.");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("코드를 다시 확인해주세요!");
+      });
   }
 }
