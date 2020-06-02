f7d7bfa2b13f85c81e3d131ac8446faa02ef815e
inkyung <inky94@naver.com>
2020-06-02 17:23:33 +0900

    S02P31A301-106 | feature/board_write_front | IK | file img preview update
    Former-commit-id: 90d62298eb8de9877cb41518a96d3dab65437f08

--- Frontend/piggylife/src/components/WriteContent/index.jsx
+++ Frontend/piggylife/src/components/WriteContent/index.jsx
@@ -50,6 +50,8 @@ class WriteContent extends React.Component{
             g_show: false,
             n_show: false,
             sid: "",
+            file: "",
+            previewURL: "",
         }
         this.g_changeColor = this.g_changeColor.bind(this);
         this.n_changeColor = this.n_changeColor.bind(this);
@@ -127,13 +129,30 @@ class WriteContent extends React.Component{
 
       g_changeColor = (e) =>{
         this.setState({g_color: e, n_color: "gray", g_show: true, n_show: false, isLike: 1,})
-    }
+      }
 
-    n_changeColor = (e) =>{
-        this.setState({n_color: e, g_color: "gray", n_show: true, g_show: false, isLike: -1,})
-    }
+      n_changeColor = (e) =>{
+          this.setState({n_color: e, g_color: "gray", n_show: true, g_show: false, isLike: -1,})
+      }
+
+      handleFileOnChange = (e) => {
+        e.preventDefault();
+        let reader = new FileReader();
+        let file = e.target.files[0];
+        reader.onloadend = () => {
+          this.setState({
+            file : file,
+            previewURL : reader.result
+          })
+        }
+        reader.readAsDataURL(file);
+      }
 
     render(){
+        let profile_preview = null;
+        if(this.state.file !== ''){
+          profile_preview = <PvImg className='profile_preview' src={this.state.previewURL}></PvImg>
+        }
         const getDetail = async (e) => {
             e.preventDefault();
             //디테일 받아올 스토어
@@ -165,8 +184,13 @@ class WriteContent extends React.Component{
 
         return(
             <Content>
-                {this.state.detail.length===0 ? (<div></div>) : (<div></div>)}
-                <Pic type="file"></Pic>
+              <PF>
+                <Pic type="file" 
+                accept='image/jpg,impge/png,image/jpeg,image/gif' 
+                name='profile_img' 
+                onChange={this.handleFileOnChange}></Pic>
+                {profile_preview}
+              </PF>
                 {this.state.show ? (
                 <ICFrame>
                     <Div onClick={() => this.g_changeColor("#5897A6")}>
@@ -260,8 +284,23 @@ const Content = styled.div`
     height: 100%;
     padding: 10% 10% 5% 10%;
 `
+
+const PF = styled.div`
+    height: auto;
+`
+const PvImg = styled.img`
+  justify-content: center;
+  align-items: center;
+  // position: absolute;
+  object-fit: cover; 
+  top: 0;
+  left: 0;
+  width: 95%;
+  height: 5rem;
+`
+
+
 const Pic = styled.input`
-    height: 5rem;
     border-style: solid;
     border-width: 0.05rem;
     border-color: gray;
