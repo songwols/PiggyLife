4d24825da0bb17fa2b396155a2af7985467eb6c0
허대원 <woob0129@naver.com>
2020-06-05 00:08:24 +0900

    S02P31A301-123 | match_back | DW | 궁합 자바 rest 제공
    Former-commit-id: b32389c04838bb8bc6197af0b3858eb30fe4cc3b

--- Backend/Python/algo/algo.py
+++ Backend/Python/algo/algo.py
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-from scipy.sparse.linalg import svds
-
-import pandas as pd
-import numpy as np
-import data
-import recommend
-
-# stores = data.getStore()
-# posts = data.getPost()
-
-# user_store_rating = posts.pivot(index='u_id', columns='s_id', values='is_like').fillna(0)
-# # pivot table 값을 numpy matrix로
-# matrix = user_store_rating.as_matrix()
-# # 사용자 평균 평점
-# user_ratings_mean = np.mean(matrix, axis=1)
-# # 사용자-가게에 대해 평균 평점 뺀 것
-# matrix_user_mean = matrix - user_ratings_mean.reshape(-1, 1)
-
-# U, sigma, Vt = svds(matrix_user_mean, k=5)
-# sigma = np.diag(sigma)
-# svd_user_predicted_ratings = np.dot(np.dot(U, sigma), Vt) + user_ratings_mean.reshape(-1, 1)
-# svd_preds = pd.DataFrame(svd_user_predicted_ratings, columns=user_store_rating.columns, index=user_store_rating.index)
-# print(svd_preds)
-# print(type(svd_preds))
-
-
-# data.save_predict(svd_preds)
-
-# df = recommend.convert_recommend(svd_preds, posts, stores)
-# print(df)
-# data.save_recommendation(df)
-
-print(recommend.convert_matching(1, 3))
-=======
 from scipy.sparse.linalg import svds
 
 import pandas as pd
@@ -54,10 +19,6 @@ user_ratings_mean = np.mean(matrix, axis=1)
 # 사용자-가게에 대해 평균 평점 뺀 것
 matrix_user_mean = matrix - user_ratings_mean.reshape(-1, 1)
 
-<<<<<<< HEAD
-print(recommend.convert_matching(1, 3))
->>>>>>> branch 'develop' of https://lab.ssafy.com/s02-final/s02p31a301.git
-=======
 U, sigma, Vt = svds(matrix_user_mean, k=5)
 sigma = np.diag(sigma)
 svd_user_predicted_ratings = np.dot(np.dot(U, sigma), Vt) + user_ratings_mean.reshape(-1, 1)
@@ -70,4 +31,3 @@ df = recommend.convert_matching(svd_preds, posts)
 data.save_matching(df)
 
 print("########## algorithm end ##########")
->>>>>>> branch 'develop' of https://lab.ssafy.com/s02-final/s02p31a301.git
--- Backend/Python/algo/data.py
+++ Backend/Python/algo/data.py
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-import pandas as pd
-import pymysql
-
-from sqlalchemy import create_engine
-
-
-def getStore():
-    print('select Store')
-
-    conn = pymysql.connect(host='52.78.169.231', user='piggy', password='piggy', db='piggy')
-    sql = "select * from store"
-    stores = pd.read_sql_query(sql, conn)
-    conn.close()
-
-    return stores
-
-def getPost():
-    print('select Post')
-    
-    conn = pymysql.connect(host='52.78.169.231', user='piggy', password='piggy', db='piggy')
-    sql = "select u_id, s_id, is_like from post where visited != 0"
-    posts = pd.read_sql_query(sql, conn)
-    conn.close()
-
-    return posts
-
-def getPredict():
-    print('select Predict')
-    
-    conn = pymysql.connect(host='52.78.169.231', user='piggy', password='piggy', db='piggy')
-    sql = "select * from predict"
-    posts = pd.read_sql_query(sql, conn, index_col='u_id')
-    conn.close()
-
-    return posts
-
-def save_recommendation(df):
-    print("insert recommendation")
-
-    engine = create_engine("mysql+pymysql://piggy:" + "piggy" + "@52.78.169.231:3306/piggy?charset=utf8", encoding='utf-8')
-    conn = engine.connect()
-
-    df.to_sql(name="recommend", con=engine, if_exists='replace', index=False)
-    
-    conn.close()
-    print("insert recommendation complete")
-
-
-def save_predict(df):
-    print("insert predict")
-    engine = create_engine("mysql+pymysql://piggy:" + "piggy" + "@52.78.169.231:3306/piggy?charset=utf8", encoding='utf-8')
-    conn = engine.connect()
-    df.to_sql(name="predict", con=engine, if_exists='replace')
-    conn.close()
-=======
 import pandas as pd
 import pymysql
 
@@ -106,10 +50,4 @@ def save_predict(df):
     engine = create_engine("mysql+pymysql://piggy:" + "piggy" + "@52.78.169.231:3306/piggy?charset=utf8", encoding='utf-8')
     conn = engine.connect()
     df.to_sql(name="predict", con=engine, if_exists='replace')
-<<<<<<< HEAD
     conn.close()
->>>>>>> branch 'develop' of https://lab.ssafy.com/s02-final/s02p31a301.git
-    print("insert predict complete")
-=======
-    conn.close()
->>>>>>> branch 'develop' of https://lab.ssafy.com/s02-final/s02p31a301.git
